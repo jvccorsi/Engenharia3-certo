40d4a62117955e9e9f5def1bc1988d03a3eb05e7
João Victor Corsi Ferreira <jvccorsiferrera@hotmail.com>
2020-10-27 20:13:36 -0300

    aa

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ include("config.php");
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!--CSS-->
-    <link rel="stylesheet" href="Css/style.css">
+    <link rel="stylesheet" href="style.css">
     <!--BS-->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 
