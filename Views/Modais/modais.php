4973610e0665a9b3def7a847eb835d73509bdf9f
João Victor Corsi Ferreira <jvccorsiferrera@hotmail.com>
2020-10-27 20:48:30 -0300

    Update modais.php

--- Views/Modais/modais.php
+++ Views/Modais/modais.php
@@ -88,12 +88,11 @@
     <div id="myModaRealizarLogin" class="modal" tabindex="-1" role="dialog">
         <div class="modal-dialog" role="document">
             <div class="modal-content">
-                <div class="modal-header bg-danger text-white">
+                <center>
+                <div class="modal-header bg-danger text-white ">
                     <h5 class="modal-title">É necessário realizar o login para acessar essa página!!</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                    </button>
                 </div>
+                </center>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-danger" data-dismiss="modal">Fechar</button>
                 </div>
