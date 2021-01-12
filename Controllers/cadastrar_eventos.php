74471440c57887576b9e8f19150a06e2d1dc9830
João Victor Corsi Ferreira <jvccorsiferrera@hotmail.com>
2021-01-12 12:15:56 -0300

    ok

--- Controllers/cadastrar_eventos.php
+++ Controllers/cadastrar_eventos.php
@@ -10,7 +10,7 @@ $obeventos= new eventos();
         }
         else{
             $_SESSION['msg'] = "<script> alert('Erro ao realizar o cadastro!'); </script>";
-            header("location:../Views/dashboard.php");
+            header("location:../Views/pages/dashboard.php");
         }
 
 ?>
