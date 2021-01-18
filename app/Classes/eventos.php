47e192eed741afcd25c51eae42ded4f4af9a5afa
Alkzv <33153364+Alkzv@users.noreply.github.com>
2021-01-17 21:57:24 -0300

    Editar evento funcionando

--- app/Classes/eventos.php
+++ app/Classes/eventos.php
@@ -1,12 +1,11 @@
 <?php
-    session_start();
-    include("../../config.php");
-    require_once ROOT_PATH . '/database/connection.php'; //Não alterar o link da conexão. Lembrando que fica salvo as alterações ;)
-    
-   
+include("../../config.php");
+require_once ROOT_PATH . '/database/connection.php';
+
 class Eventos {
     private $conexao;
     private $tabela;
+
      function __construct()
     {
         $conn = new Conexao();
@@ -26,10 +25,41 @@ public function queryInsert($dados){
        return false;
    }
  
+}
 
+public function editar_evento($dados, $id)
+{ //Alterar a nova senha do usuário        
+ try{   
+
+    $cst = $this->conexao->prepare("UPDATE evento SET nome_evento='".$dados['name']."', descricao_evento='".$dados['description']."', qtd_pessoas='".$dados['qntPeople']."', data_evento='".$dados['date']."', pais='".$dados['pais']."', cep='".$dados['cep']."', estado='".$dados['state']."', cidade='".$dados['city']."', rua='".$dados['street']."', bairro='".$dados['bairro']."', numero= '".$dados['n_local']."' WHERE id_evento = $id");
+    // $cst = $this->conexao->prepare("UPDATE $this->tabela SET nome_evento=:nome_evt,descricao_evento=:descricao_evt,qtd_pessoas=:qtd_pessoas_evt,data_evento=:data_evt,pais=:pais_evt,cep=:cep_evt,estado=:estado_evt,cidade=:cidade_evt,rua=:rua_evt,bairro=:bairro_evt,numero:=num_evt  WHERE id_evento=:user_id");
+    
+    // $cst->bindParam(":nome_evt", $dados['name'], PDO::PARAM_STR);
+    // $cst->bindParam(":descricao_evt", $dados['description'], PDO::PARAM_STR);
+    // $cst->bindParam(":qtd_pessoas_evt", $dados['qntPeople'], PDO::PARAM_STR);
+    // $cst->bindParam(":data_evt", $dados['date'], PDO::PARAM_STR);
+    // $cst->bindParam(":pais_evt", $dados['pais'], PDO::PARAM_STR);
+    // $cst->bindParam(":cep_evt", $dados['cep'], PDO::PARAM_STR);
+    // $cst->bindParam(":estado_evt", $dados['state'], PDO::PARAM_STR);
+    // $cst->bindParam(":cidade_evt", $dados['city'], PDO::PARAM_STR);
+    // $cst->bindParam(":rua_evt", $dados['street'], PDO::PARAM_STR);
+    // $cst->bindParam(":bairro_evt", $dados['bairro'], PDO::PARAM_STR);
+    // $cst->bindParam(":num_evt", $dados['n_local'], PDO::PARAM_STR);
+
+
+    // $cst->bindParam(":user_id", $id, PDO::PARAM_INT);
+
+    
+    $cst->execute();
+    return true;
+
+ }
+ catch (PDOException $ex) {
+
+    return 'error ' . $ex->getMessage();
+    
+ }
 
 }
 
-
 }
-?>
